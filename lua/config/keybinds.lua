2a8ecd32baf6f5daa0d5a36e2a8f334900ba7927
pragyann <stha.pragyan.17@gmail.com>
2026-07-18 00:15:16 +1000

    add configs for python autocompletion in

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>co", vim.cmd.copen, { desc = "Open quickfix list" })
+vim.keymap.set("n", "<leader>cc", vim.cmd.cclose, { desc = "Close quickfix list" })
