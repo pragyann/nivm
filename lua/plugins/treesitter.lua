1d5d750c1f431f7d4987d7ae6ac028faf826e8bb
pragyann <stha.pragyan.17@gmail.com>
2026-07-18 09:28:46 +1000

    use main branch for nvim-treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,60 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    branch = "master",
+    branch = "main",
     lazy = false,
     config = function()
-	require("nvim-treesitter.configs").setup({
-	    highlight = { enable = true },
-	    indent = { enable = true },
-	    -- autotag = { enable = true },
-	    ensure_installed = { "lua", "tsx", "typescript", "php", "python", "javascript", "html", "css", "json" },
-	    -- auto_install = false,
-	})
+        local parsers = {
+            "lua",
+            "tsx",
+            "typescript",
+            "php",
+            "python",
+            "javascript",
+            "html",
+            "css",
+            "json",
+            "markdown",
+            "markdown_inline",
+        }
+
+        local filetypes = {
+            "lua",
+            "typescript",
+            "typescriptreact",
+            "javascript",
+            "javascriptreact",
+            "php",
+            "python",
+            "html",
+            "css",
+            "json",
+            "markdown",
+        }
+
+        local ok, treesitter = pcall(require, "nvim-treesitter")
+
+        if ok and type(treesitter.install) == "function" then
+            treesitter.setup({
+                install_dir = vim.fn.stdpath("data") .. "/site",
+            })
+
+            treesitter.install(parsers)
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = filetypes,
+                callback = function()
+                    vim.treesitter.start() -- Enables Treesitter highlighting.
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
+            })
+        else
+            require("nvim-treesitter.configs").setup({
+                highlight = { enable = true },
+                indent = { enable = true },
+                -- Markdown parsers are needed for LSP hover windows opened by K.
+                ensure_installed = parsers,
+            })
+        end
     end
 }
