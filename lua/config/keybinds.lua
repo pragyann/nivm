b3d0e100ab293b718a4fc5967adb6eafc77b6bab
pragyann <stha.pragyan.17@gmail.com>
2026-07-24 14:11:29 +1000

    add keybind for terminal

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -2,3 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>co", vim.cmd.copen, { desc = "Open quickfix list" })
 vim.keymap.set("n", "<leader>cc", vim.cmd.cclose, { desc = "Close quickfix list" })
+vim.keymap.set("n", "<leader>tt", function()
+    vim.cmd("belowright 12split | terminal")
+    vim.cmd.startinsert()
+end, { desc = "Open terminal split" })
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
